e90d50fda6bd78389f5956207d9bd175af85aeb2
RangnathBj <120767397+RangnathBj@users.noreply.github.com>
2023-11-27 14:56:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "first1_instance" {
     ami= "ami-06d4b7182ac3480fa"  #free tier Amazon Linux 2 AMI for us-east-2
     instance_type = "t2.micro"  # Free tier eligible instance type  
     tags = {    
-        Name = "rangnath-ec2-instance"  
+        Name = "rangnathbj-ec2-instance"  
         }
     }
